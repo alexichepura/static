392fa302c5adfd73b684883d3a831f2a9dd60951
Alexi Chepura <a.chepura@gmail.com>
2017-05-14 16:08:31 +0200

    remove less

--- webpack.config.js
+++ webpack.config.js
@@ -94,33 +94,6 @@ const config = {
         NODE_ENV: JSON.stringify(NODE_ENV),
       }
     }),
-    // new LoaderOptionsPlugin({
-    //   options: {
-    //     context: __dirname,
-    //     postcss: [
-    //       require('postcss-import')(),
-    //       require('postcss-url')({
-    //         url: asset => {
-    //           let url = asset.url || asset
-    //           if (url.startsWith('http')) {
-    //             return url
-    //           }
-    //           if (!url.startsWith('/')) {
-    //             url = '/' + url
-    //           }
-    //           // full path need for development because css files lay in blobs, not urls
-    //           return IS_PROD ? url : `http://localhost:${process.env.PORT || 3000}${url}`
-    //         }
-    //       }),
-    //       require('postcss-cssnext')({
-    //         browsers: [
-    //           `last ${IS_PROD ? 2 : 1} versions`,
-    //           IS_PROD && 'iOS 7'
-    //         ].filter(p => p)
-    //       })
-    //     ]
-    //   }
-    // }),
     !IS_PROD && new HotModuleReplacementPlugin(),
     !IS_PROD && new CheckerPlugin(),
     new NamedModulesPlugin(),
